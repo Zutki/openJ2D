dc2e6c81cf93cc5d9e4f45c1266e63029dabb443
zutki <lightmeow12345@gmail.com>
2021-12-02 13:22:48 -0600

    working on block class

---
+++ Block.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Block {
+   private BufferedImage texture;
+   private Point position;
+
+   // TODO:
+   //private final int hardness = 5;
+   // ADVANCED TODO:
+   // block sound
+    
+}
