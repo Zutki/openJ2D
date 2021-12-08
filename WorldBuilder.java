9bfa5e869bbf963c872267baa1f6c75f5dc8b805
zutki <lightmeow12345@gmail.com>
2021-12-08 16:13:24 -0600

    made world builder class

---
+++ WorldBuilder.java
@@ -0,0 +1,26 @@
+import java.awt.Point;
+public class WorldBuilder {
+    private Block[][] blockArray;
+    private int terrainLevel;
+
+    public WorldBuilder(Block[][] _blockArray, int _terrainLevel) {
+        blockArray = _blockArray;
+        terrainLevel = _terrainLevel;
+    }
+
+    public Block[][] buildWorld() {
+        for (int y = blockArray.length-1; y > blockArray.length-(World.ROWS-terrainLevel)-1; y--) {
+            for (int x = 0; x < blockArray[y].length; x++) {
+                if (y == terrainLevel) {
+                    blockArray[y][x] = new Block(1, new Point(x, y));
+                }
+                else {
+                    blockArray[y][x] = new Block(0, new Point(x, y));
+                }
+            }
+        }
+
+        return blockArray;
+    }
+
+}
