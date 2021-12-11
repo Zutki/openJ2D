0355930390461f6b7359ae3da9bb5efcfd7ce0a1
Minh Nguyen <45446622+itsJustMinh@users.noreply.github.com>
2021-12-11 16:52:18 -0600

    JSONReader progress

--- Test.java
+++ Test.java
@@ -1,12 +1,7 @@
 import java.util.ArrayList;
 class Test {
     public static void main(String[] args) {
-        //Id itemID = new Id();
-        ArrayList<Object> list = new ArrayList<>();
-        list.add("hello");
-        list.add(1);
-        list.add(true);
-        list.add(1.0);
-        System.out.println(list instanceof ArrayList);
+        String str = ".8";
+        System.out.println(Double.valueOf(str));
     }
 }
--- utils/JSONReader.java
+++ utils/JSONReader.java
@@ -1,6 +1,6 @@
 package utils;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 import java.io.File;
 import java.io.FileReader;
@@ -9,57 +9,62 @@ import java.io.IOException;
 
 public class JSONReader {
     private File file;
-
-    private BufferedReader fi;
-    private HashMap<String, Object> map;
+    private ArrayList<JSONObject> args;
 
     /**
      * Constructer for JSONReader. Parses file if given file is a JSON File.
+     * 
      * @param in the JSON File
-     * @throws IOException if any errors occur while reading in JSON File
+     * @throws IOException              if any errors occur while reading in JSON
+     *                                  File
      * @throws IllegalArgumentException if given file is not a JSON File
      */
-    public JSONReader(File in)throws IOException, IllegalArgumentException{
-        if (!in.getName().matches("(\\.json)$"))
-            throw new IllegalArgumentException(String.format("File %s must be a .json file.\n", in.getName()));
-                 
+    public JSONReader(File in) throws IOException, IllegalArgumentException {
+        // file validation
+        String inName = in.getName().contains("/") ? in.getName() : "./" + in.getName();
+        if (!in.exists())
+            throw new IllegalArgumentException(String.format("File at path %s doesn't exist.", inName));
+        else if (!in.getName().matches(".json$"))
+            throw new IllegalArgumentException(String.format("File at path %s is not a JSON file", in.getName()));
+        else if (in.canRead())
+            throw new IllegalArgumentException(String.format("File at path %s cannot be read.", inName));
+
         // read in the JSON File
-        this.file = in;
-        fi = new BufferedReader(new FileReader(file));
-        map = new HashMap<>();
-        
+        file = in;
+
         // parse the file one line at a time
+        args = new ArrayList<JSONObject>();
         interpretFile(file);
     }
 
-    public Object get(String key)
-    {
-        return map.get(key);
+    /**
+     * @return all objets in the JSON file
+     */
+    public JSONObject[] getObjects() {
+        return (JSONObject[]) args.toArray();
     }
 
     /**
-     * Parses the JSON file for its keys and values and stores
-     * them into a HashMap for public access
+     * Parses the JSON file for its keys and values and stores them into a HashMap
+     * for public access
+     * 
      * @param file JSON file being read in
      * @throws IOException if error occurs while reading in JSON file
      */
-    private void interpretFile(File file)throws IOException {
-        StringBuilder key = new StringBuilder();
-        StringBuilder val = new StringBuilder();
-        boolean isStr = false;
-        boolean isKey = false;
-        int c = 0;
-  
-        // parsing the file
-        while (c != -1) {
-            c = fi.read();
-            String s = Character.toString(c);
+    private void interpretFile(File file) throws IOException {
+        BufferedReader fi = new BufferedReader(new FileReader(file));
+        StringBuilder json = new StringBuilder();
 
-            // if there is a key and value add it to the map
-            if (s.matches("\\s") && !key.isEmpty() && !val.isEmpty())
-                map.put(key.toString(), new JSONObject(val.toString()));
-            
+        String line = "";
+        while (line != null) {
+            line = fi.readLine();
+            line.replaceAll("\\s", "");
+            json.append(line);
         }
+        makeObjects(json.toString());
     }
 
+    private void makeObjects(String str) {
+        
+    }
 }
